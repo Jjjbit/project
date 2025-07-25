8e87951251772c0d31ffeb6e6dcafd455548c801
daiji <jialu.dai@edu.unifi.it>
2025-07-25 21:24:37 +0200

    add Category.java

--- src/main/java/com/ledger/domain/Category.java
+++ src/main/java/com/ledger/domain/Category.java
@@ -1,8 +1,37 @@
 package com.ledger.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Category {
-    private String name;
-    private List<SubCategory> subCategories;
+public class Category extends CategoryComponent {
+    private List<CategoryComponent> children = new ArrayList<>();
+
+    public Category(String name, String type) {
+        super(name, type);
+    }
+    @Override
+    public void remove(CategoryComponent child) {
+        children.remove(child);
+    }
+    @Override
+    public void add(CategoryComponent child) { // Aggiunge una SubCategory a Category
+        if (child instanceof SubCategory) {
+            ((SubCategory) child).setParent(this); // Imposta il parent della SubCategory
+        }
+        children.add(child); // Aggiunge una SubCategory a Category
+    }
+    @Override
+    public List<CategoryComponent> getChildren() {
+        return children;
+    }
+    public void display(String indent) {
+        System.out.println(indent + "- " + name + " (" + type + ")");
+        for (CategoryComponent child : children) {
+            child.display(indent + "  ");
+        }
+    }
+
+
 }
+
+
