ef8390d5b7692013be86a1cd6eda24f38ccfe759
daiji <jialu.dai@edu.unifi.it>
2025-07-25 21:52:35 +0200

    add CategoryComponent.java

--- src/main/java/com/ledger/domain/CategoryComponent.java
+++ src/main/java/com/ledger/domain/CategoryComponent.java
@@ -1,11 +1,12 @@
 package com.ledger.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CategoryComponent {
     protected String name;
     protected String type;
-
+    protected List<Transaction> transactions = new ArrayList<>();
 
     public CategoryComponent(String name, String type) {
         this.name = name;
@@ -15,7 +16,23 @@ public abstract class CategoryComponent {
     public abstract void add(CategoryComponent child);
     public abstract List<CategoryComponent> getChildren();
     public abstract void display(String indent);
+
     public String getType() {
         return type;
+    }
+
+    public void addTransaction(Transaction t) {
+        transactions.add(t);
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void printTransactionSummary() {
+        System.out.println("Transaction summary for: " + name);
+        for (Transaction t : transactions) {
+            System.out.println(t.getDate() + " - " + t.getAccount() + "-" + t.getAmount() + " - " + t.getDescription());
+        }
     }
 }
