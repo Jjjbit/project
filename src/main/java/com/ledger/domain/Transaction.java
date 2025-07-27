f0a9cb11d91e29d63ab0122c7d6a4b97f7c831cf
daiji <jialu.dai@edu.unifi.it>
2025-07-27 22:01:52 +0200

    add Transaction.java

--- src/main/java/com/ledger/domain/Transaction.java
+++ src/main/java/com/ledger/domain/Transaction.java
@@ -1,12 +1,39 @@
 package com.ledger.domain;
 
-public class Transaction {
-    //private LocalDate date;
-    private double amount;
-    private String description;
-    private TransactionType type;
-    private Account sourceAccount;
-    private Account destinationAccount; //only for TRANSFER
-    Category category;
-    SubCategory subCategory;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public abstract class Transaction {
+    protected LocalDate date;
+    protected BigDecimal amount;
+    protected String description;
+    protected Account account; //relaizone tra Transaction e Account è associazione
+    protected Ledger ledger;
+    protected CategoryComponent category;
+
+    public Transaction(LocalDate date, BigDecimal amount, String description, Account account, Ledger ledger, CategoryComponent category) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+        this.account = account;
+        this.ledger = ledger;
+        this.category = category;
+    }
+    public abstract void execute();
+    public LocalDate getDate() {
+        return date;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public Account getAccount() {
+        return account;
+    }
 }
+
+
+
+
