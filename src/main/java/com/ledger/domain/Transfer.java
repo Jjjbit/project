da209fef8bb0e32f36727dffe7760a4ed8fe5c77
daiji <jialu.dai@edu.unifi.it>
2025-07-27 22:03:25 +0200

    add Transfer.java

---
+++ src/main/java/com/ledger/domain/Transfer.java
@@ -0,0 +1,22 @@
+package com.ledger.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Transfer extends Transaction{
+    private Account toAccount;
+
+    public Transfer(LocalDate date, String description, Account from, Account to, BigDecimal amount, Ledger ledger) {
+        super(date, amount, description, from, ledger, null);
+        this.account = from;
+        this.toAccount = to;
+    }
+
+    public void execute() {
+        account.debit(amount);
+        toAccount.credit(amount);
+        ledger.addTransaction(this);
+    }
+}
+
+
