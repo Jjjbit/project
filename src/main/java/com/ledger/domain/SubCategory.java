7a7d92e41e7646f1eb033103a181ae88edc181cb
daiji <jialu.dai@edu.unifi.it>
2025-08-05 20:26:26 +0200

    add SubCategory.java (Entity)

--- src/main/java/com/ledger/domain/SubCategory.java
+++ src/main/java/com/ledger/domain/SubCategory.java
@@ -1,10 +1,22 @@
 package com.ledger.domain;
 
-import java.util.List;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@DiscriminatorValue("SubCategory")
 public class SubCategory extends CategoryComponent {
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
     private CategoryComponent parent;
 
+    public SubCategory() {}
     public SubCategory(String name, String type) {
         super(name, type);
     }
@@ -18,7 +30,6 @@ public class SubCategory extends CategoryComponent {
     }
     @Override
     public List<CategoryComponent> getChildren() {
-        //return Collections.emptyList(); //ritorna una lista vuota immutabile
         throw new UnsupportedOperationException("SubCategory does not support getChildren operation");
     }
     @Override
@@ -29,9 +40,34 @@ public class SubCategory extends CategoryComponent {
     public void display(String indent) {
         System.out.println(indent + "- " + name + " (" + type + ")");
     }
+    @Override
+    public List<Transaction> getTransactions() {
+        return transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getDate).reversed())
+                .collect(Collectors.toList());
+    }
+    @Override
+    public void printTransactionSummary() {
+        this.transactions= getTransactions();
+        System.out.println("Transaction summary for: " + name);
+        for (Transaction t : transactions) {
+            System.out.println(t.getDate() + " - " + t.getAccount() + "-" + t.getAmount() + " - " + t.getNote());
+        }
+    }
+
+    public void changeLevel(CategoryComponent root) {
+        if( this.getParent() != null) {
+            this.getParent().remove(this);
+            root.add(this);
+        }
+
+    }
+
     public void setParent(CategoryComponent parent) { //cambia o assegna il parent della SubCategory
+        this.parent.remove(this); // Rimuove la SubCategory dal suo parent attuale
         this.parent = parent;
     }
+
 }
 
 
