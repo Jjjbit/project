9247fb6f0c1bf9754d47dbb1b097171b2730dfcd
daiji <jialu.dai@edu.unifi.it>
2025-08-05 19:56:34 +0200

    add Ledger.java (Entity)

--- src/main/java/com/ledger/domain/Ledger.java
+++ src/main/java/com/ledger/domain/Ledger.java
@@ -1,14 +1,43 @@
 package com.ledger.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Ledger {
-    private String name;
-    private User owner;
-    private List<Transaction> transactions;
+    @Id
+    private Long id;
 
-    public Ledger(String name, User owner, List<Transaction> transactions) {
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> transactions=new ArrayList<>(); //relazione tra Transaction e Ledger è aggregazione
+
+    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BorrowingAndLending> loanRecords = new ArrayList<>(); // relazione tra LoanRecord e Ledger è aggregazione
+
+    public Ledger() {}
+    public Ledger(String name, User owner) {
         this.name = name;
         this.owner = owner;
-        this.transactions = transactions;
     }
+    public void addTransaction(Transaction tx) {
+        transactions.add(tx); // Aggiunge una transazione al ledger
+        tx.execute();
+    }
+    public void removeTransaction(Transaction tx) {
+        transactions.remove(tx); // Rimuove una transazione dal ledger
+    }
+    public void addLoanRecord(BorrowingAndLending loanRecord) {
+        loanRecords.add(loanRecord);
+    }
+
 }
+
+
