3fc15bc7733d7074c2a789087a9fb9569ef295e8
daiji <jialu.dai@edu.unifi.it>
2025-08-05 19:57:18 +0200

    add Transfer.java (Entity)

--- src/main/java/com/ledger/domain/Transfer.java
+++ src/main/java/com/ledger/domain/Transfer.java
@@ -1,22 +1,40 @@
 package com.ledger.domain;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@DiscriminatorValue("TRANSFER")
 public class Transfer extends Transaction{
+
+    @ManyToOne
+    @JoinColumn(name = "toAccount_id")
     private Account toAccount;
 
-    public Transfer(LocalDate date, String description, Account from, Account to, BigDecimal amount, Ledger ledger) {
-        super(date, amount, description, from, ledger, null);
+    public Transfer() {}
+    public Transfer(LocalDate date,
+                    String description,
+                    Account from,
+                    Account to,
+                    BigDecimal amount,
+                    Ledger ledger) {
+        super(date, amount, description, from, ledger, null, TransactionType.TRANSFER);
         this.account = from;
         this.toAccount = to;
     }
 
     public void execute() {
         if (!account.hidden && !toAccount.hidden) {
-            if (account.selectable) {
+            if (account.selectable && toAccount.selectable) {
                 account.debit(amount);
+                account.addTransaction(this);
                 toAccount.credit(amount);
+                toAccount.addTransaction(this);
                 ledger.addTransaction(this);
             }
         }
