c42cff0e780c15f98468b72ea5c3cd1989dbc070
daiji <jialu.dai@edu.unifi.it>
2025-08-05 20:26:02 +0200

    add Category.java (Entity)

--- src/main/java/com/ledger/domain/Category.java
+++ src/main/java/com/ledger/domain/Category.java
@@ -1,13 +1,34 @@
 package com.ledger.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@DiscriminatorValue("Category")
 public class Category extends CategoryComponent {
+    @OneToMany(mappedBy = "parent", cascade = jakarta.persistence.CascadeType.ALL, orphanRemoval = true)
     private List<CategoryComponent> children = new ArrayList<>();
 
+    public Category() {}
     public Category(String name, String type) {
         super(name, type);
+    }
+
+    public void changeLevel(CategoryComponent root, CategoryComponent parent) {
+        if (this instanceof Category && this.getChildren().isEmpty()) {
+            SubCategory sub = new SubCategory(this.name, this.type);
+            sub.transactions.addAll(this.getTransactions()); // Copia le transazioni dalla categoria alla subcategoria
+            parent.add(sub);
+            root.remove(this);
+        }else {
+            System.out.println("Cannot demote a category with subcategory.");
+        }
     }
 
     @Override
@@ -28,19 +49,25 @@ public class Category extends CategoryComponent {
         return children;
     }
 
+    // Ritorna la lista delle transazioni di questa categoria e delle sue subcategorie in ordine decrescente di data
+    @Override
     public List<Transaction> getTransactions() {
         List<Transaction> all = new ArrayList<>(this.transactions);
         for (CategoryComponent child : children) {
             all.addAll(child.getTransactions());
         }
-        return all;
+        return all.stream()
+                .sorted(Comparator.comparing(Transaction::getDate).reversed())
+                .collect(Collectors.toList());
     }
 
+    //stampa un riepilogo delle transazioni della categoria in ordine decrescente di data
     @Override
     public void printTransactionSummary() {
+        this.transactions = getTransactions();
         System.out.println("Transaction summary for: " + name);
-        for (CategoryComponent sub : this.children) {
-            sub.printTransactionSummary();
+        for (Transaction t : this.transactions) {
+            System.out.println(t.getDate() + " - " + t.getAccount() + " - " + t.getAmount() + " - " + t.getNote());
         }
     }
 
@@ -53,7 +80,6 @@ public class Category extends CategoryComponent {
             child.display(indent + "  ");
         }
     }
-
 
 }
 
