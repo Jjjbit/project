40a92b98fe820934cc55dbd471af46400b4f0107
daiji <jialu.dai@edu.unifi.it>
2025-08-07 18:44:12 +0200

    add UserDAO.java

---
+++ src/main/java/com/ledger/orm/UserDAO.java
@@ -0,0 +1,41 @@
+package com.ledger.orm;
+
+import com.ledger.domain.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Transactional
+public class UserDAO {
+    @PersistenceContext
+    private EntityManager em;
+
+
+    public void save(User user) {
+        em.persist(user);
+    } // Save a new user to the database
+    public User findById(Long id) {
+        return em.find(User.class, id);
+    }
+    public User findByUsername(String username) {
+        try {
+            return em.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    public void update(User user) {
+        em.merge(user);
+    }
+    public void delete(Long id) {
+        User user = em.find(User.class, id);
+        if (user != null) {
+            em.remove(user);
+        }
+    }
+
+}
