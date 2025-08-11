7c0fa3d16cbe582612e4aa7cce3d255231cfcebf
daiji <jialu.dai@edu.unifi.it>
2025-08-11 21:37:48 +0200

    add AccountDAOImpl

--- src/main/java/com/ledger/orm/AccountDAOImpl.java
+++ src/main/java/com/ledger/orm/AccountDAOImpl.java
@@ -13,13 +13,16 @@ public class AccountDAOImpl implements AccountDAO {
     @PersistenceContext
     private EntityManager em;
 
+    @Override
     public void save(Account account) {
         em.persist(account);
     }
+    @Override
     public Account findById(Long id) {
         return em.find(Account.class, id);
     }
 
+    @Override
     public List<Account> findByUserId(Long userId) {
         return em.createQuery(
                         "SELECT a FROM Account a WHERE a.owner.id = :userId AND a.hidden = false", Account.class)
@@ -27,10 +30,12 @@ public class AccountDAOImpl implements AccountDAO {
                 .getResultList();
     }
 
+    @Override
     public void update(Account account) {
         em.merge(account);
     }
 
+    @Override
     public void delete(Long id) {
         Account account = em.find(Account.class, id);
         if (account != null) {
