3afba2c68893838039f8edfd8103e0377b1807ea
daiji <jialu.dai@edu.unifi.it>
2025-10-22 18:03:45 +0200

    add BorrowingAccountAccount

---
+++ src/main/java/com/ledger/domain/BorrowingAccount.java
@@ -0,0 +1,64 @@
+package com.ledger.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+@Entity
+//@DiscriminatorValue("BORROWING")
+@Table(name = "borrowing_account")
+public class BorrowingAccount extends Account{
+
+    @Column
+    private boolean isEnded=false; //indica se il borrowing è stato completamente rimborsato
+    @Column(name="borrowing_date")
+    private LocalDate date;
+
+    public BorrowingAccount() {}
+    public BorrowingAccount(String name,  //person or entity from whom the money is borrowed
+                            BigDecimal balance, //bilancio da pagare da utente
+                            String note,
+                            boolean includedInNetWorth,
+                            boolean selectable,
+                            User owner,
+                            LocalDate date) {
+        super(name, balance, AccountType.BORROWING, AccountCategory.VIRTUAL_ACCOUNT, owner, note, includedInNetWorth, selectable);
+        this.date=date;
+    }
+
+    public void setBorrowingDate(LocalDate date){this.date=date;}
+    public void setEnded(boolean ended) {
+        isEnded = ended;
+    }
+    public boolean isEnded() {
+        return isEnded;
+    }
+    public LocalDate getBorrowingDate() {
+        return date;
+    }
+    @Override
+    public void credit(BigDecimal amount) {
+        this.balance = this.balance.subtract(amount).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public void debit(BigDecimal amount) {
+        this.balance = this.balance.add(amount).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public void repay(Transaction tx, BigDecimal amount){
+        balance = balance.subtract(amount).setScale(2, RoundingMode.HALF_UP);
+        incomingTransactions.add(tx);
+        checkAndUpdateStatus();
+    }
+
+    public void checkAndUpdateStatus() {
+        if(balance.compareTo(BigDecimal.ZERO) <= 0) {
+            this.isEnded = true;
+        } else {
+            this.isEnded = false;
+        }
+    }
+}
