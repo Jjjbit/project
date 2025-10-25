1ac55885950fd78bc5076851834a2e65dfd448d4
daiji <jialu.dai@edu.unifi.it>
2025-10-25 22:20:28 +0200

    add reset.sql

---
+++ src/test/resources/reset.sql
@@ -0,0 +1,26 @@
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+DROP TABLE IF EXISTS budgets;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS installment_plan;
+DROP TABLE IF EXISTS basic_account;
+DROP TABLE IF EXISTS credit_account;
+DROP TABLE IF EXISTS loan_account;
+DROP TABLE IF EXISTS borrowing_account;
+DROP TABLE IF EXISTS lending_account;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS ledger_categories;
+DROP TABLE IF EXISTS ledgers;
+DROP TABLE IF EXISTS global_categories;
+DROP TABLE IF EXISTS users;
+
+
+DROP SEQUENCE IF EXISTS users_seq;
+DROP SEQUENCE IF EXISTS ledgers_seq;
+DROP SEQUENCE IF EXISTS accounts_seq;
+DROP SEQUENCE IF EXISTS transactions_seq;
+DROP SEQUENCE IF EXISTS budgets_seq;
+
+
+SET REFERENTIAL_INTEGRITY TRUE;
