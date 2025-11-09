da8fc62d59ab9bce2ad77c21f3e0e9cc5566f0cf
daiji <jialu.dai@edu.unifi.it>
2025-11-09 20:03:41 +0100

    clean db

--- src/test/resources/reset.sql
+++ src/test/resources/reset.sql
@@ -1,16 +1,15 @@
+DROP TABLE IF EXISTS credit_account CASCADE;
+DROP TABLE IF EXISTS loan_account CASCADE;
+DROP TABLE IF EXISTS basic_account CASCADE;
+DROP TABLE IF EXISTS borrowing_account CASCADE;
+DROP TABLE IF EXISTS lending_account CASCADE;
+DROP TABLE IF EXISTS installment_plan CASCADE;
+DROP TABLE IF EXISTS transactions CASCADE;
+DROP TABLE IF EXISTS budgets CASCADE;
+DROP TABLE IF EXISTS ledger_categories CASCADE;
+DROP TABLE IF EXISTS ledgers CASCADE;
+DROP TABLE IF EXISTS global_categories CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 
-DROP TABLE IF EXISTS budgets;
-DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS installment_plan;
-DROP TABLE IF EXISTS basic_account;
-DROP TABLE IF EXISTS credit_account;
-DROP TABLE IF EXISTS loan_account;
-DROP TABLE IF EXISTS borrowing_account;
-DROP TABLE IF EXISTS lending_account;
-DROP TABLE IF EXISTS accounts;
-DROP TABLE IF EXISTS ledger_categories;
-DROP TABLE IF EXISTS ledgers;
-DROP TABLE IF EXISTS global_categories;
-DROP TABLE IF EXISTS users;
 
