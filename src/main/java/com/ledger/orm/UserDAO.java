98e88ef8f6ca4490917cb1d4c7d9bfe652348235
daiji <jialu.dai@edu.unifi.it>
2025-11-14 00:02:20 +0100

    remove deleteUser

--- src/main/java/com/ledger/orm/UserDAO.java
+++ src/main/java/com/ledger/orm/UserDAO.java
@@ -1,16 +1,11 @@
 package com.ledger.orm;
 
-import com.ledger.domain.Account;
-import com.ledger.domain.Budget;
-import com.ledger.domain.Ledger;
 import com.ledger.domain.User;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserDAO {
-    private Connection connection;
+    private final Connection connection;
 
     public UserDAO(Connection connection){
         this.connection = connection;
@@ -62,7 +57,11 @@ public class UserDAO {
             stmt.setLong(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
-                return buildFullUserFromResultSet(rs);
+                User user= new User();
+                user.setId(rs.getLong("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                return user;
             }
             return null;
         }
@@ -75,41 +74,15 @@ public class UserDAO {
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, username);
             ResultSet rs = stmt.executeQuery();
-            if (rs.next()) {
-                return buildFullUserFromResultSet(rs);
-            }
+                if (rs.next()) {
+                    User user= new User();
+                    user.setId(rs.getLong("id"));
+                    user.setUsername(rs.getString("username"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
             return null;
         }
-    }
-    private User buildFullUserFromResultSet(ResultSet rs) throws SQLException {
-        User user = new User();
-        user.setId(rs.getLong("id"));
-        user.setUsername(rs.getString("username"));
-        user.setPassword(rs.getString("password"));
-
-        LedgerDAO ledgerDAO = new LedgerDAO(connection);
-        AccountDAO accountDAO = new AccountDAO(connection);
-        BudgetDAO budgetDAO = new BudgetDAO(connection);
-
-        List<Ledger> ledgers = ledgerDAO.getLedgersByUserId(user.getId());
-        List<Account> accounts = accountDAO.getAccountByOwnerId(user.getId());
-        List<Budget> budgets = budgetDAO.getBudgetsByUserId(user.getId());
-
-        user.setLedgers(ledgers != null ? ledgers : new ArrayList<>());
-        user.setAccounts(accounts != null ? accounts : new ArrayList<>());
-        user.setBudgets(budgets != null ? budgets : new ArrayList<>());
-
-        for (Ledger l : user.getLedgers()) {
-            l.setOwner(user);
-        }
-        for (Account a : user.getAccounts()) {
-            a.setOwner(user);
-        }
-        for (Budget b : user.getBudgets()) {
-            b.setOwner(user);
-        }
-
-        return user;
     }
 
     @SuppressWarnings("SqlResolve")
@@ -120,18 +93,6 @@ public class UserDAO {
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getPassword());
             stmt.setLong(3, user.getId());
-
-            int affectedRows = stmt.executeUpdate();
-            return affectedRows > 0;
-        }
-    }
-
-    @SuppressWarnings("SqlResolve")
-    public boolean deleteUser(Long id) throws SQLException {
-        String sql = "DELETE FROM users WHERE id = ?";
-
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setLong(1, id);
 
             int affectedRows = stmt.executeUpdate();
             return affectedRows > 0;
